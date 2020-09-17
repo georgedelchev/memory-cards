c7e0f2d4945934d05419efafa4132413e905be62
George Delchev <delchev.official@gmail.com>
2020-09-17 16:58:06 +0300

    Initial cleanup

---
+++ src/components/CardsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardsContainer = () => {
+    return (
+        <div class="cards-grid">
+            
+        </div>
+    )
+}
+
+export default CardsContainer;
